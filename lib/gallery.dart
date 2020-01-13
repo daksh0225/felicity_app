41cd74708fcc5ed1995508588ddde09cf153fbd9
Mayank Musaddi <mayank.musaddi@research.iiit.ac.in>
2020-01-13 20:34:17 +0530

    Gallery v1

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -1,7 +1,11 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'main.dart';
 import 'drawer.dart';
+import 'imageholder.dart';
 
 class GalleryPageRoute extends CupertinoPageRoute {
 	GalleryPageRoute()
@@ -16,21 +20,81 @@ class GalleryPageRoute extends CupertinoPageRoute {
 	}
 }
 
-class GalleryPage extends StatefulWidget {
- @override
- _GalleryState createState() {
-	 return _GalleryState();
- }
+class GalleryPage extends StatelessWidget {
+
+  Widget makeImagesGrid(){
+    return GridView.builder(
+    itemCount: 12,
+    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+    itemBuilder: (context, index) {
+      return ImageGridItem(index+1);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gallery'),
+      ),
+      body: Container(
+        child: makeImagesGrid(),
+      ),
+    );
+  }
 }
 
-class _GalleryState extends State<GalleryPage> {
- @override
-	Widget build(BuildContext context) {
-	 return Scaffold(
-		 appBar: AppBar(title: Text('Gallery')),
-		body: Center(
-      child: Text('hello'),
-      ),
-		);
-	}
- }
+class ImageGridItem extends StatefulWidget {
+
+  int _index;
+  ImageGridItem(int index){
+    this._index = index;
+  }
+  @override
+  _ImageGridItemState createState() => _ImageGridItemState();
+}
+
+class _ImageGridItemState extends State<ImageGridItem> {
+
+  Uint8List imageFile;
+  StorageReference photosReference = FirebaseStorage.instance.ref().child("photos");
+
+  getImage(){
+    if(!requestedIndexes.contains(widget._index)){
+      int MAX_SIZE = 7*1024*1024;
+      photosReference.child("image_${widget._index}.jpg").getData(MAX_SIZE).then((data){
+        this.setState((){
+          imageFile = data;
+        });
+        imageData.putIfAbsent(widget._index, (){
+          return data;
+        });
+      }).catchError((error){
+        debugPrint(error.toString());
+      });
+      requestedIndexes.add(widget._index);
+    }
+  }
+  Widget decideGridFileWidget(){
+    if(imageFile==null){
+      return Center(child: Text("No Data"));
+    } else {
+      return Image.memory(imageFile,fit: BoxFit.cover);
+    }
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    if(!imageData.containsKey(widget._index)){
+      getImage();
+    } else {
+      this.setState((){
+        imageFile = imageData[widget._index];
+      });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(child: decideGridFileWidget());
+  }
+}
