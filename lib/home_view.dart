48ffc1a040d6874136d002055e09a69fc40851ce
YoogottamK <yoogottamk@outlook.com>
2020-02-08 10:43:17 +0530

    upcoming events section fixed

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -31,13 +31,13 @@ class _HomeView extends State<HomeView> {
   Widget build(BuildContext context) {
     // TODO: make this dynamic
     return Column(
-      children:<Widget>[
-        SizedBox(height: MediaQuery.of(context).size.height*0.05),
+      children: <Widget>[
+        SizedBox(height: MediaQuery.of(context).size.height * 0.05),
         Image(
           image: AssetImage('assets/felicity_logo_white.png'),
           height: 100,
         ),
-        SizedBox(height: MediaQuery.of(context).size.height*0.05),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.05),
         // Text(
         //   'Embracing the Curry Culture',
         //   textAlign: TextAlign.center,
@@ -60,114 +60,123 @@ class _HomeView extends State<HomeView> {
       // print(document.data['Date'].toDate());
       // print(DateTime.now());
       if (index < 5) {
-      // print(document.data['date'].toDate().toString()+'hello');
-      // if (document.data['date'] == date) {
+        // print(document.data['date'].toDate().toString()+'hello');
+        // if (document.data['date'] == date) {
         // print('hello'+ document['name']);
         // date = document.data['date'];
         return Container(
           margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
           child: Stack(
-          children: <Widget>[
-            Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20))
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    child: Image(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        height: MediaQuery.of(context).size.height * 0.3,
+                        image:
+                            NetworkImage(document.data['Image Poster Link']))),
               ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                child: Image(
-                  width: MediaQuery.of(context).size.width*0.7,
-                  height: MediaQuery.of(context).size.height*0.3,
-                  image: NetworkImage(document.data['Image Poster Link'])
-                )
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width*0.7,
-          child: Container(
-            // margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
-            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-            decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.6),
-              borderRadius: BorderRadius.all(Radius.circular(20))
-              // color: Colors.blue,
-            ),
-            child: Flex(
-              direction: Axis.vertical,
-              mainAxisAlignment: MainAxisAlignment.end,
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  document.data['Name'],
-                  style: TextStyle(
-                    fontSize: 25, 
-                    color: Colors.white,
-                    fontFamily: 'Qanelas'
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: Container(
+                  // margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.6),
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                      // color: Colors.blue,
+                      ),
+                  child: Flex(
+                    direction: Axis.vertical,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        document.data['Name'],
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontFamily: 'Qanelas'),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            EvaIcons.pin,
+                            color: Colors.red,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            document.data['Venue'],
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            EvaIcons.calendar,
+                            color: Colors.amber,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            DateFormat("dd/MM/yyyy")
+                                .format(document.data['Date'].toDate())
+                                .toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Icon(
+                            EvaIcons.clockOutline,
+                            color: Colors.blue,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            DateFormat("HH:mm")
+                                .format(document.data['Date']
+                                    .toDate()
+                                    .subtract(Duration(minutes: 330)))
+                                .toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                 ),
-                SizedBox(height: 20,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                Icon(
-                  EvaIcons.pin,
-                  color: Colors.red,
-                ),
-                SizedBox(width: 10,),
-                Text(
-                  document.data['Venue'],
-                  style: TextStyle(
-                    color: Colors.white
-                  ),
-                ),
-                  ],
-                ),
-                SizedBox(height: 10,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(
-                      EvaIcons.calendar,
-                      color: Colors.amber,
-                    ),
-                    SizedBox(width: 5,),
-                    Text(
-                      DateFormat("dd/MM/yyyy")
-                          .format(document.data['Date'].toDate())
-                          .toString(),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(width: 20,),
-                    Icon(
-                      EvaIcons.clockOutline,
-                      color: Colors.blue,
-                    ),
-                    SizedBox(width: 5,),
-                    Text(
-                      DateFormat("HH:mm")
-                          .format(document.data['Date'].toDate().subtract(Duration(minutes: 330)))
-                          .toString(),
-                      style: TextStyle(color: Colors.white),
-                    ),                  
-                  ],
-                )
-              ],
-            ),
-          ),
-        ),
-          ],
+              ),
+            ],
           ),
         );
-      // }
-      // else{
-      //   return Container(
-      //     child: Text('nothing to show'),
-      //   );
+        // }
+        // else{
+        //   return Container(
+        //     child: Text('nothing to show'),
+        //   );
       }
     }
     // print(DateTime.now().day);
@@ -190,29 +199,27 @@ class _HomeView extends State<HomeView> {
                   'Upcoming Events',
                   textAlign: TextAlign.left,
                   style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontFamily: 'Qanelas'
-                    // color: Color(0xffff0000),
-                  ),
+                      fontSize: 20, color: Colors.white, fontFamily: 'Qanelas'
+                      // color: Color(0xffff0000),
+                      ),
                 ),
-                Icon(EvaIcons.arrowIosForward,
-                  color: Colors.white
-                )
+                Icon(EvaIcons.arrowIosForward, color: Colors.white)
               ],
             ),
-          // decoration: BoxDecoration(
-          //   // color: Color(0xff50B1FB), 
-          //   color: Color.fromARGB(255, 150, 7, 69),
-          //   // color: Colors.white,
-          //   borderRadius: BorderRadius.all(Radius.circular(10))
-          // ),
-          // margin: EdgeInsets.fromLTRB(5, 5, 5, 20),
-        ),
+            // decoration: BoxDecoration(
+            //   // color: Color(0xff50B1FB),
+            //   color: Color.fromARGB(255, 150, 7, 69),
+            //   // color: Colors.white,
+            //   borderRadius: BorderRadius.all(Radius.circular(10))
+            // ),
+            // margin: EdgeInsets.fromLTRB(5, 5, 5, 20),
+          ),
           StreamBuilder(
               stream: Firestore.instance
                   .collection('test')
-                  .orderBy('Date').where('Date', isGreaterThan: DateTime.now())
+                  .orderBy('Date')
+                  .where('Date',
+                      isGreaterThan: DateTime.now().add(Duration(minutes: 330)))
                   .snapshots(),
               builder: (context, snapshot) {
                 if (!snapshot.hasData) {
@@ -229,12 +236,12 @@ class _HomeView extends State<HomeView> {
                   height: 200,
                   margin: EdgeInsets.all(20),
                   child: new Center(
-                    child:ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: snapshot.data.documents.length,
-                    itemBuilder: (context, index) =>
-                        builder(index, snapshot.data.documents[index]),
-                  ),
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (context, index) =>
+                          builder(index, snapshot.data.documents[index]),
+                    ),
                   ),
                 );
               }),
